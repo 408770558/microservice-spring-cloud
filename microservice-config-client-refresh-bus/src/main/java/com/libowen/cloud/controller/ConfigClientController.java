03af3c4c64e617dd656bd7ffe4fd8d030625aba2
408770558 <a408770558@qq.com>
2019-07-31 00:37:29 +0800

    config自动刷新

--- microservice-config-client-refresh-bus/src/main/java/com/libowen/cloud/controller/ConfigClientController.java
+++ microservice-config-client-refresh-bus/src/main/java/com/libowen/cloud/controller/ConfigClientController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
  * 手动刷新 localhost:8089/actuator/refresh
  * bus手动刷新地址 http://localhost:8089/actuator/bus-refresh
  * bus自动刷新 在github中添加Webhooks 每次push时自动触发一个http请求到http://localhost:8089/actuator/bus-refresh   需外网地址
+ * /bus/refresh?destination=customers:9000   单个刷新
+ * /bus/refresh?destination=customers:**    刷新所有名称为customers的application id
+ *
  * @author Bowen.Li@onerway.com
  * @date 2019/7/30
  * 说明：
